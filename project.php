87f14eb0c5bd7780d352c796285962cc99e82914
Samarthgawade19 <samarthgawade9@gmail.com>
2025-05-30 11:19:00 +0530

    some change

--- project.php
+++ project.php
@@ -9,7 +9,7 @@
         <h5 class="text-danger">Our Projects</h5>
         <h1>Our Projects Inspiration for Your Next Project</h1>
         <div class="card mt-3" style="width: 18rem;">
-            <img src="PRIMECONSTRUCT/img/1.jpg" class="card-img-top" alt="error">
+            <img src="img/11.jpg" class="card-img-top" alt="error">
             <div class="card-body">
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
                     card’s content.</p>
