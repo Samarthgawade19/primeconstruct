d4d3e9ed4edd2836d5158ec4d24c8ea0cadc47bf
ManthanG121 <manthangawade007@gmail.com>
2025-05-31 10:55:54 +0530

    .....

--- header.php
+++ header.php
@@ -4,37 +4,37 @@
   integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
 
-<div class="row text-center pt-2" style="background-color:black; height: 100px;">
+<div class="row text-center pt-2" style="background-color:black; height: 150px;">
   <h2 style="color:white">
     primeconstruct
   </h2>
   <hr style="color: white;">
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-      <li>
-        <a aria-current="page" href="index.php" style="color:white">Home</a>
-      </li>
-      <li>
-        <a href="#" style="color:white">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"
-          style="color:white">
-          Dropdown
-        </a>
-        <ul class="dropdown-menu">
-          <li><a class="dropdown-item" href="#">Action</a></li>
-          <li><a class="dropdown-item" href="#">Another action</a></li>
-          <li>
-            <hr class="dropdown-divider">
-          </li>
-          <li><a class="dropdown-item" href="#">Something else here</a></li>
-        </ul>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-      </li>
-    </ul>
+  <div>
+    <nav class="navbar navbar-expand-lg bg-blac">
+      <div class="container-fluid">
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" style="color:white" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" style="color:white" href="about.php">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" style="color:white" href="contact.php">Contact Us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" style="color:white" aria-disabled="true">Disabled</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   </div>
 </div>
