ceb04d342145c69339531be9da3c729f1d471850
ManthanG121 <manthangawade007@gmail.com>
2025-05-31 18:03:39 +0530

    ......

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 include "header.php";
 ?>
+<div class="row">
+    <div class="col-12">
+        <img src=".\img\main_img.webp" alt="" class="col-12">
+    </div>
+</div>
+<?php
+include "footer.php";
+?>
